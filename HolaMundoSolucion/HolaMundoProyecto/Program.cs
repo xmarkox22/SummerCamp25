b308134151a78748efd0e80af0665b53525e102f
Pedro Hernandez <hispafox@gmail.com>
2025-07-16 11:36:32 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("============= ");
+
+        Console.WriteLine(@" ___ ___   ___ __ _        ___ ___ | | __ _ ");
+        Console.WriteLine(@"/ __/ _ \ / __/ _` |_____ / __/ _ \| |/ _` |");
+        Console.WriteLine(@" (_| (_) | (_| (_| |_____| (_| (_) | | (_| |");
+        Console.WriteLine(@"\___\___/ \___\__,_|      \___\___/|_|\__,_|");
+        Console.WriteLine(@"soft drink - coke - The Coca-Cola Company ");
+
+        Console.WriteLine(@"         __                              ___   __        .ama     ,");
+        Console.WriteLine(@"      ,d888a                          ,d88888888888ba.  ,88""I)   d");
+        Console.WriteLine(@"     a88']8i                         a88"".8""8)   `""8888:88  "" _a8'");
+        Console.WriteLine(@"   .d8P' PP                        .d8P'.8  d)      ""8:88:baad8P'");
+        Console.WriteLine(@"  ,d8P' ,ama,   .aa,  .ama.g ,mmm  d8P' 8  .8'        88):888P'");
+        Console.WriteLine(@" ,d88' d8[ ""8..a8""88 ,8I""88[ I88' d88   ]IaI""        d8[         ");
+        Console.WriteLine(@" a88' dP ""bm8mP8'(8'.8I  8[      d88'    `""         .88          ");
+        Console.WriteLine(@",88I ]8'  .d'.8     88' ,8' I[  ,88P ,ama    ,ama,  d8[  .ama.g");
+        Console.WriteLine(@"[88' I8, .d' ]8,  ,88B ,d8 aI   (88',88""8)  d8[ ""8. 88 ,8I""88[");
+        Console.WriteLine(@"]88  `888P'  `8888"" ""88P""8m""    I88 88[ 8[ dP ""bm8m88[.8I  8[");
+        Console.WriteLine(@"]88,          _,,aaaaaa,_       I88 8""  8 ]P'  .d' 88 88' ,8' I[");
+        Console.WriteLine(@"`888a,.  ,aadd88888888888bma.   )88,  ,]I I8, .d' )88a8B ,d8 aI");
+        Console.WriteLine(@"  ""888888PP""'        `8""""""8   ""888PP'  `888P'  `88P""88P""8m""");
+    }
+}
