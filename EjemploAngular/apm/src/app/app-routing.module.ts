3c9c0a9327b60f288e8969b7616949fefaf001e5
eve <jami.159261@e.unavarra.es>
2025-08-20 11:09:07 +0200

    Mis cambios locales

--- EjemploAngular/apm/src/app/app-routing.module.ts
+++ EjemploAngular/apm/src/app/app-routing.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PeliculasComponent } from './peliculas/peliculas.component';
-import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { CursoDetalleComponent } from './curso-detalle/curso-detalle.component';
+import { Peliculas } from './peliculas/peliculas';
 import { DemoFormReactivoComponent } from './demo-form-reactivo/demo-form-reactivo.component';
 
 const routes: Routes = [
-
-    { path: 'cursos', component: CursosListaComponent },
-    { path: 'peliculas', component: PeliculasComponent },
-    { path: 'curso-detalle/:id', component: CursoDetalleComponent },
-    { path: 'demo-form-reactivo', component: DemoFormReactivoComponent },
-    { path: 'demo-form-registro', component: DemoFormRegistroComponent },
-
-    { path: '', component: WelcomeComponent },
-    { path: '**', redirectTo: '' }
+  { path: 'peliculas', component: Peliculas },
+  { path: 'demo-form-reactivo', component: DemoFormReactivoComponent },
+  { path: '', component: WelcomeComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- EjemploAngular/apm/src/app/app.module.ts
+++ EjemploAngular/apm/src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CounterdemoComponent } from './counterdemo/counterdemo.component';
 import { CardDemoComponent } from './card-demo/card-demo.component';
-import { PeliculasComponent } from './peliculas/peliculas.component';
+import { Peliculas } from './peliculas/peliculas';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
-import { CursoDetalleComponent } from './curso-detalle/curso-detalle.component';
 import { DemoFormReactivoComponent } from './demo-form-reactivo/demo-form-reactivo.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -19,9 +18,7 @@ import { DemoFormReactivoComponent } from './demo-form-reactivo/demo-form-reacti
     WelcomeComponent,
     CounterdemoComponent,
     CardDemoComponent,
-    PeliculasComponent,
-    CursosListaComponent,
-    CursoDetalleComponent,
+    Peliculas,
     DemoFormReactivoComponent
   ],
   imports: [
@@ -29,7 +26,7 @@ import { DemoFormReactivoComponent } from './demo-form-reactivo/demo-form-reacti
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-     ReactiveFormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
